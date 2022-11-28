d8e7d2779c85f9480531442c84a8f4709a8b531d
Luke Watson-Davies <lukew@cogapp.com>
2022-11-28 16:18:17 +0000

    using local JSON for staff data

--- src/api.ts
+++ src/api.ts
@@ -1,27 +1,13 @@
-import { config } from "@/config";
 import { staffResponse, Staff } from "@/interfaces";
 
-const allStaffQuery = `
-    query {
-        staff {
-          name
-          flash
-          image
-          sound
-          daysWorked
-        }
-    }
-`;
-
 const requestOptions = {
-  method: "POST",
+  method: "GET",
   headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ query: allStaffQuery }),
 };
 
 export default {
   async fetchStaff(): Promise<Staff[]> {
-    const response = await fetch(config.graphQLAPI, requestOptions);
+    const response = await fetch("/staff.json", requestOptions);
     if (response.status !== 200) throw new Error(response.statusText);
     const { data } = (await response.json()) as staffResponse;
     return data.staff;
--- src/config.ts
+++
@@ -1,7 +0,0 @@
-const config = {
-  useGraphQL: true,
-  graphQLAPI: "https://staffql.herokuapp.com/graphql",
-  // graphQLAPI: "http://localhost:4000/graphql",
-};
-
-export { config };
